98b2db6fd8990e6bdaaa7455f597103007caf1a2
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2022-12-26 02:32:13 -0500

    1 file modified

--- app.vue
+++ app.vue
@@ -4,8 +4,13 @@
       <a class="text-rose-500" href="/">Home</a>
       <a class="text-teal-400" href="/about">About</a>
       <a class="text-orange-600" href="/blog">Blog</a>
-      <a class="text-" href="/contact">Contact</a>
+      <a class="text-emerald-700" href="/contact">Contact</a>
     </nav>
-    <p class="p-2 text-lg">Hello world, my name is Andrew Weisbeck. I am a full-stack web developer. Come learn morea about how I write code for Nuxt3 Web Applications.</p>
+    <h1 class="pl-2 pt-10 text-teal-700 text-4xl">Learn Nuxt3 on AppCity</h1>
+    <p class="pl-2 pt-5 text-sky-800 font-bold text-lg">Hello world, my name is Andrew Weisbeck. I am a full-stack web developer. Come learn more about how I write code for Nuxt3 Web Applications.</p>
+  </div>
+  <div>
+    <h2 class="pl-2 pt-10 text-red-500 font-bold text-3xl">There's More to Come!</h2>
+    <p class="pl-2 pt-5 text-emerald-800 font-bold text-lg">I'm so excited about all the content that will go here. It will be about things that are thing slinging thing and a dinga ding ding. Bing bing bing and a trinket ta ting.</p>
   </div>
 </template>
