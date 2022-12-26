ba5fa8642a87b1c0e970c4fc8116e435cc02a27c
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2022-12-26 02:47:21 -0500

    1 file modified

--- app.vue
+++ app.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <nav>
-      <a class="text-rose-500 hover:text-emerald-700" href="/">Home</a>
-      <a class="text-teal-400 hover:text-yellow-400" href="/about">About</a>
-      <a class="text-orange-600 hover:text-sky-800" href="/blog">Blog</a>
-      <a class="text-emerald-700 hover:text-sky-300" href="/contact">Contact</a>
+      <a class="text-rose-500 hover:text-emerald-700 pl-5" href="/">Home</a>
+      <a class="text-teal-400 hover:text-yellow-400 pl-5" href="/about">About</a>
+      <a class="text-orange-600 hover:text-sky-800 pl-5" href="/blog">Blog</a>
+      <a class="text-emerald-700 hover:text-sky-300 pl-5" href="/contact">Contact</a>
+      <a class="bg-emerald-300 text-slate-900 p-2 border-solid-3 border-black ml-8 rounded-lg hover:bg-sky-400 hover:text-white" href="/contact">Send Message</a>
     </nav>
     <h1 class="pl-2 pt-10 text-teal-700 text-4xl">Learn Nuxt3 on AppCity</h1>
     <p class="pl-2 pt-5 text-sky-800 font-bold text-lg">Hello world, my name is Andrew Weisbeck. I am a full-stack web developer. Come learn more about how I write code for Nuxt3 Web Applications.</p>
